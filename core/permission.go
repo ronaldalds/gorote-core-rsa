ca4a66c283df9545b470587229a07e57e7f00f16
ronaldalds <43586601+ronaldalds@users.noreply.github.com>
2025-04-28 14:30:08 -0300

    Update permission.go

--- core/permission.go
+++ core/permission.go
@@ -3,12 +3,14 @@ package core
 type PermissionCode string
 
 const (
-	PermissionSuperUser            PermissionCode = "super_user"
-	PermissionCreateUser           PermissionCode = "create_user"
-	PermissionViewUser             PermissionCode = "view_user"
-	PermissionUpdateUser           PermissionCode = "update_user"
-	PermissionEditePermissionsUser PermissionCode = "edite_permissions_user"
-	PermissionCreateRole           PermissionCode = "create_role"
-	PermissionViewRole             PermissionCode = "view_role"
-	PermissionUpdateRole           PermissionCode = "update_role"
+	PermissionSuperUser        PermissionCode = "super_user"
+	PermissionCreateUser       PermissionCode = "create_user"
+	PermissionViewUser         PermissionCode = "view_user"
+	PermissionUpdateUser       PermissionCode = "update_user"
+	PermissionCreatePermission PermissionCode = "create_permission"
+	PermissionViewPermission   PermissionCode = "view_permission"
+	PermissionUpdatePermission PermissionCode = "update_permission"
+	PermissionCreateRole       PermissionCode = "create_role"
+	PermissionViewRole         PermissionCode = "view_role"
+	PermissionUpdateRole       PermissionCode = "update_role"
 )
