e173e39cec3e98718487aa49771866f33fbdc653
ronaldalds <43586601+ronaldalds@users.noreply.github.com>
2025-09-15 17:30:02 -0300

    update docker e func db

--- gorote/db.go
+++ gorote/db.go
@@ -120,11 +120,11 @@ func NewRedis(url string) (*redis.Client, error) {
 	return client, nil
 }
 
-func NewGorm(dialector gorm.Dialector) (*gorm.DB, error) {
+func NewGorm(dialector gorm.Dialector, opt ...gorm.Option) (*gorm.DB, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	db, err := gorm.Open(dialector, &gorm.Config{})
+	db, err := gorm.Open(dialector, opt...)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to the database: %v", err)
 	}
